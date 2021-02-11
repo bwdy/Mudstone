f75f7459d9214aeb662d59c82b2330c83cf8cba3
bwdy <47769190+bwdymods@users.noreply.github.com>
2021-02-10 23:51:45 -0900

    Comment how this works so I don't forget

--- src/main/java/wtf/bwdy/mudstone/mixin/SimpleInventoryMixin.java
+++ src/main/java/wtf/bwdy/mudstone/mixin/SimpleInventoryMixin.java
@@ -8,6 +8,9 @@ import wtf.bwdy.mudstone.MudstoneMod;
 
 @Mixin(SimpleInventory.class)
 abstract class SimpleInventoryMixin {
+    // edit the 'transfer' method in SimpleInventory to redirect the call
+    // to Inventory.getMaxCountPerStack() [which we can't edit, because its a default interface method]
+    // to our own static function
     @Redirect(method = "transfer", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/SimpleInventory;getMaxCountPerStack()I"))
     private int redirectGetMaxCountPerStack(SimpleInventory callingInventory) {
         return MudstoneMod.getStackSizeforStackSize(callingInventory.getMaxCountPerStack());
